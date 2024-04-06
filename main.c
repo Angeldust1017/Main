f5f116a47e82227a90e8e76ba364c39da4372ba6
Angeldust1017 <angeldust1017@outlook.es>
2024-04-05 22:58:26 -0600

    git push origin

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
-/* Programa: Hola mundo (Versión 2) */
+/* Programa: Calcular la devaluación de un automovil por año, teniendo en cuenta la recuperación de 2000 dólares*/
 #include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
-    printf( "\n   Hola mundo." );
-    printf( "\n\n   Pulse una tecla para salir..." );
+    int p_car, dev_car, rec_car, ad_car; p_car = 20000, rec_car = 2000;
+    setlocale(LC_ALL,"spanish");
 
+
+    printf( "\n  El precio del carro es:\n %d dolares", p_car);
+    printf( "\n Ingrese los annos de devaluacion del carro:");
+    scanf("%d",&dev_car);
+    printf("\n El monto de recuperacion del carro es de: %d dolares \n",rec_car);
+ad_car= (p_car-rec_car); ad_car = (ad_car/dev_car);
+    printf("\n El carro se va devaluando: \n %d dolares por anno",ad_car);
     getch(); /* Pausa */
 
     return 0;
