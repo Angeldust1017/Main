46d49fd7728b00220a61262288356d408ce16ff0
Angeldust1017 <128069403+Angeldust1017@users.noreply.github.com>
2024-05-10 15:54:07 -0600

    main.c

--- main.c
+++ main.c
@@ -1,23 +1,67 @@
-/* Programa: Calcular la devaluación de un automovil por año, teniendo en cuenta la recuperación de 2000 dólares*/
+/* Programa: Detecta que signo zodiacal es el usuario*/
 #include <stdio.h>
-#include <conio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <locale.h>
 
-int main()
-{
-    int p_car, dev_car, rec_car, ad_car; p_car = 20000, rec_car = 2000;
-    setlocale(LC_ALL,"spanish");
+int main() {
+    int dia; int mes;
+    printf("*** Signo Zodiacal ***\n");
+    printf("Ingrese su fecha de nacimiento solo dia y mes \n");
+    printf("Ingrese el dia en formato de numero \n");
+    scanf("%d", &dia);
+    printf("Ingrese el mes en formato de numero \n");
+    scanf("%d", &mes);
 
+    if( dia == 0 || mes <= 0 || dia > 31 || mes > 12) {
+        printf("ingreso un dato incorrecto en día y/o mes");
+    }
+    else {
+             if( mes == 1 && dia >= 20 || mes == 2 && dia <= 18){
+                 printf("Eres Acuario ");
 
-    printf( "\n  El precio del carro es:\n %d dolares", p_car);
-    printf( "\n Ingrese los annos de devaluacion del carro:");
-    scanf("%d",&dev_car);
-    printf("\n El monto de recuperacion del carro es de: %d dolares \n",rec_car);
-ad_car= (p_car-rec_car); ad_car = (ad_car/dev_car);
-    printf("\n El carro se va devaluando: \n %d dolares por anno",ad_car);
-    getch(); /* Pausa */
+             }
+             if( mes == 2 && dia >= 19 || mes == 3 && dia <= 20){
+             printf("Eres Piscis ");
+             }
 
+             if( mes == 3 && dia >= 21 || mes == 4 && dia <= 19){
+              printf("Eres Aries ");
+              }
+
+              if( mes == 4 && dia >= 20 || mes == 5 && dia <= 20){
+              printf("Eres Tauro ");
+               }
+
+              if( mes == 5 && dia >= 21 || mes == 6 && dia <= 20){
+               printf("Eres Geminis ");
+                }
+
+        if( mes == 6 && dia >= 21 || mes == 7 && dia <= 22){
+            printf("Eres Cancer ");
+        }
+
+        if( mes == 7 && dia >= 23 || mes == 8 && dia <= 22){
+            printf("Eres Leo ");
+        }
+
+        if( mes == 8 && dia >= 23 || mes == 9 && dia <= 22){
+            printf("Eres Virgo ");
+        }
+
+        if( mes == 9 && dia >= 23 || mes == 10 && dia <= 22){
+            printf("Eres Libra ");
+        }
+
+        if( mes == 10 && dia >= 23 || mes == 11 && dia <= 21){
+            printf("Eres Escorpio ");
+        }
+
+        if( mes == 11 && dia >= 22 || mes == 12 && dia <= 21){
+            printf("Eres Sagitario ");
+        }
+
+        if( mes == 12 && dia >= 22 || mes == 1 && dia <= 18){
+            printf("Eres Capricornio ");
+        }
+
+    }
     return 0;
 }
